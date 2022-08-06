6d91ba1b6b49e6982979f3f1760f62f60ec3f0e3
Еlena Anisimova <elena.anis.12@yandex.ru>
2022-08-06 13:39:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = "";
+    let morse = "";
+    for (let i = 0; i < expr.length; i += 10) {
+        for (let k = i; k != (i + 10) ; k += 2) {
+            if ((expr[k] === "0") && (expr[k + 1] === "0")) { continue; }
+            if ((expr[k] === "1") && (expr[k + 1] === "0")) { morse +="."; continue;}
+            if ((expr[k] === "1") && (expr[k + 1] === "1")) { morse +="-"; continue;}
+            if (expr[k] === "*") { res+=" "; break; }
+        }
+        if (morse != "") { res += MORSE_TABLE[morse]; }
+        morse = "";
+    }
+    return res;
 }
 
 module.exports = {
